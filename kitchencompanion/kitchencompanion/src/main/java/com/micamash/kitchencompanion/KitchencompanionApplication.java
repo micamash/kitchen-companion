4c333ce5b77c9f2b9aa3ec959af4aeae8bd5dda6
Ashley Mical <smashlandi@gmail.com>
2023-05-04 14:54:52 -0500

    Created project files

---
+++ kitchencompanion/kitchencompanion/src/main/java/com/micamash/kitchencompanion/KitchencompanionApplication.java
@@ -0,0 +1,13 @@
+package com.micamash.kitchencompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchencompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchencompanionApplication.class, args);
+	}
+
+}
---
+++ kitchencompanion/kitchencompanion/src/test/java/com/micamash/kitchencompanion/KitchencompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micamash.kitchencompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchencompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
