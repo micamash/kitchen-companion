2960785343fbfeb90bdfed78f02de5f58fad3d21
Ashley Mical <smashlandi@gmail.com>
2023-05-05 15:20:31 -0500

    Completed database design in kitchen-companion-database.sql

--- kitchencompanion/kitchencompanion/kitchen-companion-database.sql
+++ kitchencompanion/kitchencompanion/kitchen-companion-database.sql
@@ -1,25 +1,98 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS food_item, inventory, recipe, shopping_list, meal_plan CASCADE;
+DROP TABLE IF EXISTS meal_plan_recipe;
+DROP TABLE IF EXISTS shopping_list;
+DROP TABLE IF EXISTS recipe_item;
+DROP TABLE IF EXISTS meal_plan;
+DROP TABLE IF EXISTS shopping_list;
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS inventory;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE food_item(
-	food_item_id serial,
+CREATE TABLE item (
+	item_id serial,
+	name varchar(200) NOT NULL,
 	storage_location varchar(200) NOT NULL,
-	expiration_date date NOT NULL,
+	expiration_date date,
+	item_size varchar(200),
 	image_name varchar(200),
-	item_size varchar(200)
-	foreword_by int,
-	CONSTRAINT PK_food_item PRIMARY KEY(food_item_id),
-    CONSTRAINT FK_food_item_recipe FOREIGN KEY(recipe_id) REFERENCES recipe(recipe_id)
+	CONSTRAINT PK_item PRIMARY KEY(item_id)
 );
 
-CREATE TABLE recipe(
-	recipe_id serial,
-	recipe_name varchar(200) NOT NULL,
-	recipe_description varchar(200) NOT NULL,
-	cuisine_type varchar(200)  NOT NULL,
-	image_name varchar(200),
-	servings int,
-	cook_time time 
-	CONSTRAINT PK_recipe PRIMARY KEY(recipe_id),
+CREATE TABLE users (
+	user_id serial,
+	username varchar(15),
+	password_hash varchar(200),
+	role varchar,
+	CONSTRAINT PK_user PRIMARY KEY(user_id)
 );
+
+CREATE TABLE inventory (
+	inventory_id serial,
+	user_id int NOT NULL,
+	item_id int NOT NULL,
+	quantity int NOT NULL,
+	date_added date,
+	CONSTRAINT PK_inventory PRIMARY KEY(inventory_id),
+	CONSTRAINT FK_user FOREIGN KEY(user_id) REFERENCES users(user_id),
+	CONSTRAINT FK_item FOREIGN KEY(item_id) REFERENCES item(item_id)
+);
+
+CREATE TABLE recipe (
+	recipe_id serial,
+	user_id int,
+	recipe_name varchar(200) NOT NULL,
+	cuisine_type varchar(200)  NOT NULL,
+	description varchar(200) NOT NULL,
+	servings int,
+	cook_minutes int,
+	instruction varchar(5000),
+	image_name varchar(200),
+	CONSTRAINT PK_recipe PRIMARY KEY(recipe_id),
+	CONSTRAINT FK_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE recipe_item (
+	recipe_id int,
+	item_id int,
+	CONSTRAINT PK_recipe_item PRIMARY KEY(recipe_id, item_id),
+	CONSTRAINT FK_recipe FOREIGN KEY(recipe_id) REFERENCES recipe(recipe_id),
+	CONSTRAINT FK_item FOREIGN KEY(item_id) REFERENCES item(item_id)
+);
+
+CREATE TABLE shopping_list (
+	shopping_list_id serial,
+	user_id int,
+	name varchar(200),
+	date_created date,
+	CONSTRAINT PK_shopping_list PRIMARY KEY(shopping_list_id),
+	CONSTRAINT FK_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE shopping_list_item (
+	shopping_list_id int,
+	item_id int,
+	CONSTRAINT PK_shopping_list_item PRIMARY KEY(shopping_list_id, item_id),
+	CONSTRAINT FK_shopping_list FOREIGN KEY(shopping_list_id) REFERENCES shopping_list(shopping_list_id),
+	CONSTRAINT FK_item FOREIGN KEY(item_id) REFERENCES item(item_id)
+);
+
+CREATE TABLE meal_plan (
+	meal_plan_id serial,
+	user_id int,
+	name varchar(200),
+	date_created date,
+	CONSTRAINT PK_meal_plan PRIMARY KEY(meal_plan_id),
+	CONSTRAINT FK_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE meal_plan_recipe (
+	meal_plan_id int,
+	recipe_id int,
+	CONSTRAINT PK_meal_plan_recipe PRIMARY KEY(meal_plan_id, recipe_id),
+	CONSTRAINT FK_meal_plan FOREIGN KEY(meal_plan_id) REFERENCES meal_plan(meal_plan_id),
+	CONSTRAINT FK_recipe FOREIGN KEY(recipe_id) REFERENCES recipe(recipe_id)
+);
+
+COMMIT;
